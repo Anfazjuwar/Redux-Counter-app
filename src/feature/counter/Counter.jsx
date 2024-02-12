c54f39908934196418460aa171a1b40cf5a90062
Anfazjuwar <anfazjuwar@gmail.com>
2024-02-12 22:28:50 +0530

    Studying Redux

---
+++ src/feature/counter/Counter.jsx
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
+//useselector means geting pacific  state data from the store  and using it in our component
+//useDispatch means doing actions
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const [incrementAmount, SetIncrementAmount] = useState(0);
+
+  const addvalue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    SetIncrementAmount(0);
+    dispatch(reset());
+  };
+
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => SetIncrementAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addvalue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset All</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
